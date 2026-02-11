9da2ae6c8a8b931c8d780375f4d952ab988060e3
anike <aniketpandey798@gmail.com>
2026-02-11 10:15:27 +0530

    Setup routing configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,21 @@
-import { useEffect, useState } from 'react'
-import Counter from './components/Counter'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Users from './pages/Users'
+import UserDetails from './pages/UserDetails'
 
 function App() {
-  const [users, setUsers] = useState([])
-  const [count, setCount] = useState(0)
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
-  const [search, setSearch] = useState('')
-
-  useEffect(() => {
-    console.log("Component mounted");
-  }, [])
-
-
-  useEffect(() => {
-  const fetchUsers = async () => {
-    try {
-      const res = await fetch('https://jsonplaceholder.typicode.com/users')
-
-      if (!res.ok) {
-        throw new Error('Something went wrong')
-      }
-
-      const data = await res.json()
-      setUsers(data)
-    } catch (err) {
-      setError(err.message)
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  fetchUsers()
-}, [])
-
-const filteredUsers = users.filter(user =>
-  user.name.toLowerCase().includes(search.toLowerCase())
-)
-
-  if(loading)
-    return <h2>Loading users...</h2>
-    if(error)
-      return <h2>Error: {error}</h2>
-
   return (
     <div>
-    <div>
-      <h1>User List</h1>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/users">Users</Link>
+      </nav>
 
-      <input
-        type="text"
-        placeholder="Search users..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-
-      <ul>
-        {filteredUsers.map(user => (
-          <li key={user.id}>
-            {user.name} – {user.email}
-          </li>
-        ))}
-      </ul>
-    </div>
-    <div>
-      <h1>useEffect - Run Once</h1>
-      <p>Count : {count}</p>
-      <button onClick = {() => setCount(count +1)}>+</button>
-    </div>
-    <div>
-      <h1>Reusable Counter</h1>
-      <Counter
-       value ={count}
-       onIncrement = {() => setCount(count + 1)}
-      />
-    </div>
-    <h1>User List</h1>
-    <ul>
-      {users.map(user => (
-        <li key={user.id}>
-          {user.name} - {user.email}
-        </li>
-      ))};
-    </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/users/:id" element={<UserDetails />} />
+      </Routes>
     </div>
   )
 }
